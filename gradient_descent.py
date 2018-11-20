3511165a40fe047363a2dd3af12916c864a9e249
bendavies <benpd@mac.com>
2018-11-20 20:35:58 +0000

    updated gradient descent

--- gradient_descent.py
+++ gradient_descent.py
@@ -6,7 +6,7 @@ import gravity_model
 mass_coeff = 2
 deter_coeff = 5
 
-distances, populations, OD = gravity_model.main(6, 1000, mass_coeff, deter_coeff)
+distances, populations, OD = gravity_model.main(10, 1000, mass_coeff, deter_coeff)
 
 
 def prob_ij(i, j, distances, populations, mass_co, deter_co):
@@ -18,11 +18,13 @@ def prob_ij(i, j, distances, populations, mass_co, deter_co):
 
     for n in range(nlocs):
         if i != n:
-            totals[n] = np.exp(mass_co*np.log(populations[n]) - distances[i, n]/deter_co)
+#            totals[n] = np.exp(mass_co*np.log(populations[n]) - distances[i, n]/deter_co)
+            totals[n] = np.exp(mass_co*np.log(populations[n]) - distances[i, n] * deter_co)
     total = sum(totals)
 
 
-    value = np.exp(mass_co*np.log(populations[j]) - distances[i, j]*(1/deter_co))
+#    value = np.exp(mass_co*np.log(populations[j]) - distances[i, j]*(1/deter_co))
+    value = np.exp(mass_co*np.log(populations[j]) - distances[i, j]*(deter_co))
     if i == j:
         value = 0
     prob = value/total
@@ -59,9 +61,10 @@ def gradient_deterco(OD, distances, populations, mass_co, deter_co):
         for j in range(nlocs):
             first += (OD[i, j] * distances[i, j])
         for l in range(nlocs):
-            second +=  (prob_ij(i, l, distances, populations, mass_co, deter_co) * distances[i, l])
+            second += (prob_ij(i, l, distances, populations, mass_co, deter_co) * distances[i, l])
         second = sum(OD[i]) * second
-        value = first - second
+#        value = first + second
+        value = - first + second
         deter_grad += value
 
     return deter_grad
@@ -75,9 +78,9 @@ def likelihood_grad(populations, distances, pop_co, deter_co):
     return params_grad
 
 
-params = np.array((3,4))
-learning_rate = 0.01
-epochs = 50
+params = np.array((1., 1. / 5))
+learning_rate = 5e-6
+epochs = 5000
 convergence1 = np.zeros(epochs)
 convergence2 = np.zeros(epochs)
 beta = mass_coeff * np.ones(epochs)
@@ -92,7 +95,10 @@ for i in range(epochs):
     convergence2[i] = params[1]
 
     params_grad = likelihood_grad(populations, distances, params[0], params[1])
-    delta = (learning_rate * params_grad[0], learning_rate * params_grad[1])
+    delta = np.array([learning_rate * params_grad[0], learning_rate * params_grad[1]])
+    if i%50 == 0:
+        print(params)
+#        print([params_grad, delta])
     # velocity[layer] = gamma * velocity[layer] + alpha * grad[layer]
     # velocity1[i + 1] = gamma * velocity1[i] + learning_rate * params_grad[0]
     # velocity2[i + 1] = gamma * velocity2[i] + learning_rate * params_grad[1]
@@ -106,8 +112,8 @@ for i in range(epochs):
 
 print(params)
 
-plt.plot(range(epochs), convergence1)
-plt.plot(range(epochs), convergence2)
+plt.plot(range(epochs), convergence1, '.-')
+plt.plot(range(epochs), 1. / convergence2, '.-')
 plt.plot(range(epochs), beta, 'g--')
 plt.plot(range(epochs), R, 'g--')
 plt.show()
