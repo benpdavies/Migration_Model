3032bdc4fcb2b77b4d99dffc8f21d4a0bb8599c0
bendavies <benpd@mac.com>
2018-12-11 10:19:42 +0000

    neural net

--- custom_nn.py
+++ custom_nn.py
@@ -10,7 +10,7 @@ nlocs = 10
 max_pop = 1000
 
 # Create synthetic data
-distances, populations, OD = gravity_model.main(nlocs, max_pop, mass_coeff, deter_coeff)
+locations, distances, populations, OD = gravity_model.main(nlocs, max_pop, mass_coeff, deter_coeff)
 
 # Create new data without origin-origin journeys
 new_distances = np.zeros((nlocs, nlocs-1))
@@ -31,54 +31,50 @@ for i in range(nlocs):
         x[i,j,1] = new_distances[i,j]
 
 
-x = x.transpose(0,1,2).reshape(90, x.shape[2])
-new_OD = np.ravel(new_OD)
+# x = x.transpose(0,1,2).reshape(90, x.shape[2])
+# new_OD = np.ravel(new_OD)
 
-# X = 90x2 vector with destination x population
+# X = 10x9x2 vector with destination x population
 X = torch.tensor(x, dtype=torch.float)
-# y = 90x1 vector with total journeys
+# y = x1 vector with total journeys
 y = torch.tensor(new_OD, dtype=torch.float)
 
 
-class TwoLayerNet(torch.nn.Module):
-    def __init__(self, D_in=90):
-        """
-        In the constructor we instantiate two nn.Linear modules and assign them as
-        member variables.
-        """
-        super(TwoLayerNet, self).__init__()
-        self.linear1 = torch.nn.Linear(D_in, D_in)
-        self.linear2 = torch.nn.Linear(D_in, D_in, bias=True)
-        self.softmax = torch.nn.Softmax()
+class GravityModel_MultinomialRegression(torch.nn.Module):
+
+    def __init__(self, dim_features=2):
+        super(GravityModel_MultinomialRegression, self).__init__()
+        self.linear1 = torch.nn.Linear(dim_features, 1)
 
     def forward(self, X):
-        """
-        In the forward function we accept a Tensor of input data and we must return
-        a Tensor of output data. We can use Modules defined in the constructor as
-        well as arbitrary operators on Tensors.
-        """
+        out = self.linear1(X)
+        return out
 
-        sum = self.linear1(X[:,0]) + self.linear2(X[:,1])
-        # sum = self.linear1(X)
-        y_pred = self.softmax(sum)
-        return y_pred
 
-model = TwoLayerNet()
+    def loss(out, y):
+        lsm = torch.nn.LogSoftmax(dim=0)
+        return - ( y * lsm(torch.squeeze(out)) ).sum()
 
-criterion = torch.nn.MSELoss(reduction='sum')
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
+model = GravityModel_MultinomialRegression()
 
-for t in range(500):
+# criterion = torch.nn.MSELoss(reduction='sum')
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-1)
+
+for t in range(100000):
+
     # Forward pass: Compute predicted y by passing x to the model
     y_pred = model(X)
 
     # Compute and print loss
-    loss = criterion(y_pred, y)
-    # print(t, loss.item())
+    loss = GravityModel_MultinomialRegression.loss(y_pred, y)
+    if t % 1000 == 0:
+        print(t, loss.item())
 
     # Zero gradients, perform a backward pass, and update the weights.
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-
-print(model.linear1.weight.data.norm())
+#
+# print(model.linear1.weight.data.norm())
+# print(y_pred)
+# print(y)
