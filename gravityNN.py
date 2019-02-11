db033d28c423f13c72938b3dfe057b9c36e32a48
bendavies <benpd@mac.com>
2019-02-11 17:52:44 +0000

    Latest changes to linear neural network

--- gravityNN.py
+++ gravityNN.py
@@ -3,6 +3,7 @@ import numpy as np
 import gravity_model
 import random
 import matplotlib.pyplot as plt
+from torch.autograd import Variable
 
 
 class GravityModel_MultinomialRegression(torch.nn.Module):
@@ -10,7 +11,7 @@ class GravityModel_MultinomialRegression(torch.nn.Module):
     def __init__(self, dim_features=2):
         super(GravityModel_MultinomialRegression, self).__init__()
         self.linear1 = torch.nn.Linear(dim_features, 1)
-        self.relu1 = torch.nn.ReLU()
+        # self.relu1 = torch.nn.ReLU()
 
     def forward(self, X):
         out = self.linear1(X)
@@ -27,17 +28,21 @@ class GravityModel_MultinomialRegression(torch.nn.Module):
         return - (y * lsm(torch.squeeze(out))).sum()
 
 model = GravityModel_MultinomialRegression()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.00001)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
 
-epochs = 1000
-batches = 1000
+epochs = 100
+batches = 200
 parames = []
 
 for epoch in range(epochs):
 
     X, y = gravity_model.create_training_data(nlocs=batches, max_popn=1000, mass_co=2, deterrance_co=2)
 
-    # print(y)
+    if epoch == 0:
+        print(np.shape(X))
+        # shape(X) = [batches, (batches-1), 2]
+        print(np.shape(y))
+        # shape(Y) = [batches, (batches-1)]
 
     for batch in range(batches):
 
@@ -56,24 +61,21 @@ for epoch in range(epochs):
         loss = model.loss(outputs, trainJourneys)
 
         # Backward pass
-        optimizer.zero_grad()
         loss.backward()
+
         # Update weights
         optimizer.step()
 
-        # print(trainData)
-        # print(trainJourneys)
-        # print(y_pred)
-
         params = list(model.parameters())
         # print(params[0][0].detach().numpy())
-        parames.append(loss.item())
+        parames.append(params[0][0].detach().numpy()[0])
 
 
     if epoch % 1 == 0:
         print(epoch, loss.item())
+        print(params[0][0].detach().numpy())
 
-# print(parames)
-
-# plt.plot(parames)
-# plt.show()
+print(parames)
+#
+plt.plot(parames)
+plt.show()
